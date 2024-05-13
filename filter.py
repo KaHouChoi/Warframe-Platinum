c892c2e6a08a9a4da330b8a7be4cb393a8d766a6
root <root@DESKTOP-BKPGIQD>
2024-05-13 14:35:59 +0800

    turned json output to dictionary

--- filter.py
+++ filter.py
@@ -15,3 +15,23 @@ def filter():
     with open("items.txt", "w") as f:
         for i in x:
             print(i, file=f)
+    
+    items = {}
+    dict_filter = re.compile(r'''
+        ^\A'thumb':\s'(.+\bpng)
+        .+\bid':\s'(.+)
+        ',\s'url_name':\s'(.+)
+        ',\s'item_name':\s'(.+)'
+    ''',re.VERBOSE                       
+    )                       
+    f = open("items.txt", "r")
+    while line := f.readline():
+        mo = dict_filter.search(line)
+        if mo: 
+            temp_dict = {
+                "thumb": mo.group(1),
+                "id": mo.group(2),
+                "url_name": mo.group(3)
+            }
+            items[mo.group(4)] = temp_dict
+    return items
