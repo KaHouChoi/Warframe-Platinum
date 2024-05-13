7c9b82c8201d01e31d8fb3a6c94b0a643edd084c
root <root@DESKTOP-BKPGIQD>
2024-05-13 12:36:53 +0800

    put call in a callable function

--- call.py
+++ call.py
@@ -1,6 +1,7 @@
 import requests
 
-response = requests.get("https://api.warframe.market/v1/items") 
+def call():
+    response = requests.get("https://api.warframe.market/v1/items") 
 
-with open("items.txt", "w") as f:
-  print(response.json(), file=f)
+    with open("items.txt", "w") as f:
+      print(response.json(), file=f)
